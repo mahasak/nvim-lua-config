f6ebac71427d5f17042feeef8983275db0f636b8
Max Pijittum <mpijittum@meta.com>
2023-08-16 16:00:17 +0800

    add configs for lsp servers and ntree

--- lua/max/plugins/lsp/mason.lua
+++ lua/max/plugins/lsp/mason.lua
@@ -20,26 +20,24 @@ end
 mason.setup()
 
 mason_lspconfig.setup({
-	-- list of servers for mason to install
 	ensure_installed = {
 		"tsserver",
 		"html",
+		"dart",
+		"solidity",
 		"cssls",
 		"tailwindcss",
 		"lua_ls",
 		"emmet_ls",
 	},
-	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
 })
 
 mason_null_ls.setup({
-	-- list of formatters & linters for mason to install
 	ensure_installed = {
 		"prettier", -- ts/js formatter
 		"stylua", -- lua formatter
 		"eslint_d", -- ts/js linter
 	},
-	-- auto-install configured formatters & linters (with null-ls)
 	automatic_installation = true,
 })
--- lua/max/plugins/nvim-tree.lua
+++ lua/max/plugins/nvim-tree.lua
@@ -1,8 +1,23 @@
 -- import nvim-tree plugin safely
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-  return
+	return
 end
+
+local function open_nvim_tree()
+	require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	nested = true,
+	callback = function()
+		if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+			vim.cmd("quit")
+		end
+	end,
+})
 
 -- recommended settings from nvim-tree documentation
 vim.g.loaded_netrw = 1
@@ -10,31 +25,26 @@ vim.g.loaded_netrwPlugin = 1
 
 -- change color for arrows in tree to light blue
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
-
 -- configure nvim-tree
 nvimtree.setup({
-  -- change folder arrow icons
-  renderer = {
-    icons = {
-      glyphs = {
-        folder = {
-          arrow_closed = "", -- arrow when folder is closed
-          arrow_open = "", -- arrow when folder is open
-        },
-      },
-    },
-  },
-  -- disable window_picker for
-  -- explorer to work well with
-  -- window splits
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = false,
-      },
-    },
-  },
-  -- 	git = {
-  -- 		ignore = false,
-  -- 	},
+	renderer = {
+		icons = {
+			glyphs = {
+				folder = {
+					arrow_closed = "+", -- arrow when folder is closed
+					arrow_open = "-", -- arrow when folder is open
+				},
+			},
+		},
+	},
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+	git = {
+		ignore = false,
+	},
 })
